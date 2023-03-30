32e65a28123c5d013582dda86e901b8fe0749e4e
dharanicb <dharanicb86391@gmail.com>
2023-03-30 04:07:52 +0000

    solution recent

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Button = (props) => {
   //  Write your code here.
   const { className, buttonText } = props;
-  return;
-  <button className={`${className}`}> {buttonText} </button>;
+  return <button className={`${className}`}> {buttonText} </button>;
 };
 
 const element = (
@@ -10,9 +9,11 @@ const element = (
   <div className="container">
     <div className="flex-container">
       <h1 className="heading">Social Buttons</h1>
-      <Button buttonText="Like" className="btn like-btn" />
-      <Button buttonText="Comment" className="btn comment-btn" />
-      <Button buttonText="Share" className="btn share-btn" />
+      <div className="flex-content">
+        <Button buttonText="Like" className="btn like-btn" />
+        <Button buttonText="Comment" className="btn comment-btn" />
+        <Button buttonText="Share" className="btn share-btn" />
+      </div>
     </div>
   </div>
 );
